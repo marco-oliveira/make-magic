897d7e8506617d1ee1eb95c5e8613868df7f1a7b
Marco Antônio <marco-oliveira@live.com>
2020-08-16 18:56:34 -0300

    add model dto mapper for Character

---
+++ src/main/java/com/marco/makemagic/api/dto/CharacterDTO.java
@@ -0,0 +1,102 @@
+package com.marco.makemagic.api.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class CharacterDTO implements Serializable {
+
+    private static final long serialVersionUID = -6612009885576934568L;
+
+    @ApiModelProperty(value = "Id do Personagem")
+    private Long id;
+
+    @ApiModelProperty(value = "Nome do Personagem")
+    @NotBlank
+    @Size(max = 100, min = 1)
+    private String name;
+
+    @ApiModelProperty(value = "Função do Personagem")
+    @NotBlank
+    @Size(max = 100, min = 1)
+    private String role;
+
+    @ApiModelProperty(value = "Escola do Personagem")
+    @NotBlank
+    @Size(max = 100, min = 1)
+    private String school;
+
+    @NotBlank
+    @Size(max = 100, min = 1)
+    @ApiModelProperty(value = "Casa do Personagem")
+    private String house;
+
+    @NotBlank
+    @Size(max = 100, min = 1)
+    @ApiModelProperty(value = "Patrono do Personagem")
+    private String patronus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getPatronus() {
+        return patronus;
+    }
+
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CharacterDTO that = (CharacterDTO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/marco/makemagic/api/model/Character.java
@@ -0,0 +1,92 @@
+package com.marco.makemagic.api.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "characters")
+public class Character implements Serializable {
+
+    private static final long serialVersionUID = -707640578066717982L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column(length = 100)
+    private String role;
+
+    @Column(length = 100)
+    private String school;
+
+    @Column(length = 100)
+    private String house;
+
+    @Column(length = 100)
+    private String patronus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getPatronus() {
+        return patronus;
+    }
+
+    public void setPatronus(String patronus) {
+        this.patronus = patronus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Character character = (Character) o;
+        return Objects.equals(id, character.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
