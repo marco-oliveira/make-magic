77fb9b1dc35e73133e82d831dc0a4aefcab3cd7e
Marco Antônio <marco-oliveira@live.com>
2020-08-16 18:43:13 -0300

    add dependencies in pom.xml

---
+++ src/main/java/com/marco/makemagic/api/MakeMagicApplication.java
@@ -0,0 +1,13 @@
+package com.marco.makemagic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeMagicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marco/makemagic/api/MakeMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.makemagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMagicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
